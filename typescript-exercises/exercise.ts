7001453de80010b0176d69c35c8093e196f84e3f
Valerie Song <valsong88@gmail.com>
2025-07-08 21:02:41 -0400

    exercise done

--- typescript-exercises/exercise.ts
+++ typescript-exercises/exercise.ts
@@ -1,6 +1,29 @@
 // Create your interfaces and enum below:
+interface Task {
+    id: number;
+    title: string; 
+    description: string; 
+    dueDate: Date;
+}
 
+enum Priority {
+    LOW, 
+    MEDIUM,
+    HIGH
+}
+
+interface TaskPriority extends Task {
+    priority: Priority;
+}
 // Create your task below:
 
+const task: TaskPriority = {
+    priority: Priority.LOW,
+    id: 1,
+    title: "Hi",
+    description: "hi",
+    dueDate: new Date()
+};
+
 // ONCE DONE, UNCOMMENT THE LINE BELOW AND RUN THE FILE
-// console.log(task);
+console.log(task);
